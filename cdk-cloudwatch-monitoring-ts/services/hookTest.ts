163a1209e4d40c926e2f18ddeb163c569f15b8af
yishai <yishai5555@gmail.com>
2025-07-11 16:59:20 +0300

    04-ts

---
+++ cdk-cloudwatch-monitoring-ts/services/hookTest.ts
@@ -0,0 +1,11 @@
+import { handler } from './hook';
+
+handler({
+  Records: [
+    {
+      Sns: {
+        Message: 'Test message',
+      },
+    },
+  ],
+} as any);
---
+++ cdk-cloudwatch-monitoring-ts/services/secret.ts
@@ -0,0 +1,2 @@
+export const url =
+  'https://hooks.slack.com/';
