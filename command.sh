0a82dd3be8a720b73f0bbf11a5edb815fa4e18a2
yishai <yishai5555@gmail.com>
2025-07-18 17:56:03 +0300

    10-commands-for-trgger

--- command.sh
+++ command.sh
@@ -1,3 +1,8 @@
 aws cloudwatch put-metric-data --namespace YyCustom --metric-name custom-error --value 1
 
 aws cloudwatch describe-alarms --alarm-name TestAlarm
+
+
+aws cloudwatch put-metric-data --namespace Custom --metric-name custom-error --value 1
+aws cloudwatch put-metric-data --namespace Custom --metric-name custom-error --value 10
+aws cloudwatch put-metric-data --namespace Custom --metric-name custom-error --value 100
