2752a64a411ad30c7a1a739bf6d4998a1afb13ae
yishai <yishai5555@gmail.com>
2025-07-18 16:19:20 +0300

    07-ts-fix-2

--- cdk-cloudwatch-monitoring-ts/lib/cdk-cloudwatch-monitoring-ts-stack.ts
+++ cdk-cloudwatch-monitoring-ts/lib/cdk-cloudwatch-monitoring-ts-stack.ts
@@ -8,7 +8,7 @@ export class CdkCloudwatchMonitoringTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const webHookLambda = new NodeJsFunction(this, 'webHookLambda', {
+    const webHookLambda = new NodejsFunction(this, 'webHookLambda', {
         runtime:Runtime.NODEJS_18_X,
         handler: 'handler',
         entry: (Join(__dirname, '..', 'services', 'handler.ts'))
