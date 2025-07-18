2eb58dbcbc5719d60946598ff36527804cb2020a
yishai <yishai5555@gmail.com>
2025-07-18 16:48:21 +0300

    08-clone-to-handler.ts

---
+++ cdk-cloudwatch-monitoring-ts/services/handler.ts
@@ -0,0 +1,21 @@
+import { SNSEvent } from 'aws-lambda';
+
+import { url as webhookUrl } from './secret';
+
+async function handler(event: SNSEvent) {
+  for (const record of event.Records) {
+    const message = record.Sns.Message;
+
+    await fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text: `Huston, we have a problem: ${message}`,
+      }),
+    });
+  }
+}
+
+export { handler };
